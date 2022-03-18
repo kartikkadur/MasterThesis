954f539f5e51ca425a9044a000590ca7fbd40c1d
kartikkadur <50872230+kartikkadur@users.noreply.github.com>
2022-03-18 11:14:32 +0100

    Update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python train.py --dataroot $1 --model BaseModel --dataset PairedDataset --batch_size 1 --num_workers 1 --use_dis_content --num_domains 4 --concat --reparam
+python train.py --dataroot $1 --model AdaINModel --dataset PairedDataset --batch_size 1 --num_workers 1 --use_dis_content --num_domains 4 --concat --reparam
